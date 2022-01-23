994bfec8b89a8cffa98e9c405795ee8bd3a21feb
rusZovnet3 <cferls_ru@hotmail.com>
2022-01-23 12:49:26 -0400

    Control del boton toggle de cambios de color de fondo en CSS y JS. v1.0.3

--- assets/js/app.js
+++ assets/js/app.js
@@ -25,4 +25,18 @@
 		});
 		//console.log(listaPeli[i].computedStyleMap().get("transform")[0].x.value);
 		//console.log(listaPeli[i].querySelectorAll("img").length);
+		console.log(Math.floor(window.innerWidth / 270));
+	});
+
+
+
+	//Control de toggle
+	const ball = document.querySelector(".toggle-ball");
+	const items = document.querySelectorAll(".container,.movie-list-title,.navbar-container,.sidebar,.left-menu-icon,.toggle");
+
+	ball.addEventListener("click", () => {
+	  items.forEach((item) => {
+	    item.classList.toggle("active");
+	  })
+	  ball.classList.toggle("active");
 	});
